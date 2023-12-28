e363a4c6b01634745fa130a9032eb5297c44b86d
Rajesh RN <quic_rajeshra@quicinc.com>
2023-12-28 15:36:54 +0530

    msmnile_gvmq_s_u: Changes for new lunch msmnile_gvmq_s_u
    Changes for new lunch combo msmnile_gvmq_s_u
    
    Change-Id: I677f64908e605349ef8f0d5ba90c614726bd0010
    Signed-off-by: Rajesh RN <quic_rajeshra@quicinc.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/msmnile_gvmq_s_u.mk
+COMMON_LUNCH_CHOICES := \
+    msmnile_gvmq_s_u-userdebug \
+    msmnile_gvmq_s_u-user
---
+++ msmnile_gvmq_s_u.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/qcom/msmnile_gvmq/msmnile_gvmq.mk)
+PRODUCT_NAME := msmnile_gvmq_s_u
+PRODUCT_DEVICE := msmnile_gvmq_s_u
+PRODUCT_BRAND := qti
+PRODUCT_MODEL := msmnile_gvmq_s_u for arm64
+TARGET_BOARD_SUFFIX := _gvmq
+TARGET_BOARD_DERIVATIVE_SUFFIX := _s_u
+DEVICE_MANIFEST_FILE := device/qcom/msmnile_gvmq/manifest.xml
+
+SHIPPING_API_LEVEL := 32
