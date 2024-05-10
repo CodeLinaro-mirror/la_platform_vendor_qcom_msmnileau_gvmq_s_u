81e2fe3060935d491f34e7dfe9c1ac1bc7720a58
LADI RAM SAI <quic_lramsai@quicinc.com>
2024-05-10 12:06:24 +0530

    [msmnile_gvmq_s_u]: Boot-KPI Optimizations.
    Update the androidboot parameter for parallel load modules
    in kernel cmndline.
    
    Signed-off-by: LADI RAM SAI <quic_lramsai@quicinc.com>
    Change-Id: I6f9e1a708148ca6d4e530ad4d8a682b8a98bcff2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,7 +198,7 @@ TARGET_USES_QCOM_BSP := false
 
 #BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.selinux=enforcing androidboot.memcg=1 androidboot.recover_usb=1 androidboot.dtbo_idx=1
 
-BOARD_KERNEL_CMDLINE := debug user_debug=31 loglevel=9 print-fatal-signals=1  init=/init swiotlb=4096  kpti=0 pcie_ports=compat firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.hardware=qcom androidboot.selinux=enforcing androidboot.memcg=1 androidboot.recover_usb=1 androidboot.dtbo_idx=1
+BOARD_KERNEL_CMDLINE := debug user_debug=31 loglevel=9 print-fatal-signals=1  init=/init swiotlb=4096  kpti=0 pcie_ports=compat firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.hardware=qcom androidboot.selinux=enforcing androidboot.memcg=1 androidboot.recover_usb=1 androidboot.dtbo_idx=1 androidboot.load_modules_parallel=true
 
 ifeq ($(TARGET_CONSOLE_ENABLED),true)
 BOARD_KERNEL_CMDLINE += console=hvc0,115200
