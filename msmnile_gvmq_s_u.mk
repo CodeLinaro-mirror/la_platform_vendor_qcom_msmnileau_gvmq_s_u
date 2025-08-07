a9258d280775235a6c2d98a5d73027fad541d9f3
Seereddi Chandra Sekhar <quic_seereddi@quicinc.com>
2025-08-07 16:10:47 +0530

    Added property to enable Asymmetric Slot switch feature
    Added  ro.vendor.asymmetric_support property
    
    Change-Id: I4fd6403fbf2a71efe2f3b7fd16f7c8ede90cda41
    Signed-off-by: Seereddi Chandra Sekhar <quic_seereddi@quicinc.com>

--- msmnile_gvmq_s_u.mk
+++ msmnile_gvmq_s_u.mk
@@ -776,6 +776,13 @@ PRODUCT_VENDOR_PROPERTIES += persist.vendor.car.lpm=true
 # default wifi country code
 PRODUCT_VENDOR_PROPERTIES += ro.boot.wificountrycode=us
 
+#Make this flag false if Symmetric slot switch is used
+TARGET_USES_ASYMMETRIC_SLOT_SWITCH := true
+
+ifeq ($(TARGET_USES_ASYMMETRIC_SLOT_SWITCH), true)
+  PRODUCT_PROPERTY_OVERRIDES += ro.vendor.asymmetric_support=true
+endif
+
 # The property "persist.bluetooth.enablenewavrcp" is introduced in AOSP.
 # See commit e63f6d6bda16bd94d43537fc5db754a103c6a757
 # (1) If the property is set as true, it indicates that AVRCP(TG) is enabled.
